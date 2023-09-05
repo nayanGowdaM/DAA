8ab3e59cefde26b5eb23f50777e70fb47770dee7
nayanGowdaM <mnayangowda@gamil.com>
2023-09-06 01:10:08 +0530

    Heap Sort

--- HeapSort.c
+++ HeapSort.c
@@ -1,30 +1,40 @@
 #include <stdio.h>
-#define SIZE 100
-int a[SIZE];
-void heapify(int arr[], int n, int i) {
-    int largest = i; 
-    int left = 2 * i + 1;
-    int right = 2 * i + 2; 
-    if (left < n && arr[left] > arr[largest]) 
-        largest = left;
-    if (right < n && arr[right] > arr[largest]) 
-        largest = right;
-    if (largest != i) {
-        int temp = arr[i];
-        arr[i] = arr[largest];
-        arr[largest] = temp;
-        heapify(arr, n, largest);
+#include <stdlib.h>
+
+void heapify(int a[],int n)
+{
+    int i,k,v,j,flag=0;
+    for(i=n/2;i>=1;i--)
+    {
+        k=i;
+        v=a[k];
+        while(!flag && 2*k <= n)
+        {
+            j=2*k;
+            if(j<n)
+            {
+                if(a[j]<a[j+1])
+                    j=j+1;
+            }
+            if(v>=a[j])
+                flag=1;
+            else
+            {
+                a[k]=a[j];
+                k=j;
+            }
+        }
+        a[k]=v;
+        flag=0;
     }
 }
 
-void heapSort(int arr[], int n) {
-    for (int i = n / 2 - 1; i >= 0; i--) 
-        heapify(arr, n, i);
-    for (int i = n - 1; i >= 0; i--) {
-        int temp = arr[0];
-        arr[0] = arr[i];
-        arr[i] = temp;
-        heapify(arr, i, 0);
+void heapSort(int a[],int n){
+    for(int i=n;i>=1;i--){
+        int t=a[i];
+        a[i]=a[1];
+        a[1]=t;
+        heapify(a,i-1);
     }
 }
 
@@ -32,11 +42,13 @@ int main(){
     int n;
     printf("Enter the number of elements: ");
     scanf("%d",&n);
+    int a[n];
     printf("Enter the elements of array:\n");
-    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    for(int i=1;i<=n;i++) scanf("%d",&a[i]);
+    heapify(a,n);
     heapSort(a,n);
     printf("Sorted array is: ");
-    for(int i=0;i<n;i++) printf("%d\t",a[i]);
+    for(int i=1;i<=n;i++) printf("%d\t",a[i]);
     printf("\n");
     return 0;
 }
