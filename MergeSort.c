7def5d06a01194f20f282665cabe37e92173f984
nayanGowdaM <mnayangowda@gamil.com>
2023-09-06 00:34:44 +0530

    Merge Sort

--- MergeSort.c
+++ MergeSort.c
@@ -1,37 +1,47 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
-#define SIZE 256
-int table[256];
-void shiftTable(char p[]){
-    int m=strlen(p);
-    for(int i=0;i<SIZE;i++) table[i]=m;
-    for(int i=0;i<m-1;i++) table[p[i]] = m-i-1;
+int ct=0;
+#define N 1000
+int a[N];
+void merge(int l, int mid, int r){
+    int b[r-l+2];
+    int i=l,j=mid+1,k=0;
+    while(i<=mid && j<=r){
+        if(a[i]<a[j]){
+            b[k++]=a[i++];
+        }
+        else b[k++]=a[j++];
+        ct++;
 
+    }
+    while(i<=mid){
+        b[k++]=a[i++];
+        ct++;
+    } 
+    while(j<=r) {
+        b[k++]=a[j++];
+        ct++;
+    }
+    for(i=l;i<=r ; i++) a[i]=b[i-l];
 }
 
-int Horspool(char t[],char p[]){
-    shiftTable(p);
-    int m=strlen(p),n=strlen(t);
-    int i=m-1;
-    while(i<n){
-        int k=0;
-        while(k<m && p[m-k-1]==t[i-k]) k++;
-        if(k==m) return i-k+1;
-        else i=i+table[t[i]];
-    }
-    return -1;
+void mergeSort(int l, int r){
+    if(l==r) return ;
+    int mid=(l+r)/2;
+    mergeSort(l,mid);
+    mergeSort(mid+1,r);
+    merge(l,mid,r);
 }
 
 int main(){
-    char t[SIZE], p[SIZE];
-    printf("Enter the Pattern and Text strings\n");
-    // fgets(p,SIZE,stdin);
-    // fgets(t,SIZE, stdin);
-    scanf("%s",t);
-    scanf("%s",p);
-    int i=Horspool(t,p);
-    if(i==-1) printf("\nPattern Not Found");
-    else printf("\nPattern found at %d",i);
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d",&n);
+    printf("Enter the elements of array:\n");
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    mergeSort(0,n-1);
+    printf("Sorted array is: ");
+    for(int i=0;i<n;i++) printf("%d\t",a[i]);
+    printf("\n");
     return 0;
 }
